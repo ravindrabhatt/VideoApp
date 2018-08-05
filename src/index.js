cb4874aa508adb87d3e84ae0ae934e826b413391
Ravindra Bhatt <ravindra.bhatt29@gmail.com>
2018-08-05 10:12:26 +0530

    Use class based component instead for function based components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-const API_KEY='AIzaSyD0iMMTaC_i3CymzBLYNlgMHfNSb50tBbU';
+const API_KEY='';
 
-const App = () => {
-	return (
-		<div>
-		  <SearchBar />
-		</div>  
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+    YTSearch({ key: API_KEY, term:'Kpop' }, (videos) => {
+  		this.setState({ videos });
+    });
+	}
+
+	render(){
+	  return (
+		  <div>
+		    <SearchBar />
+		  </div>  
+	  );
+  }
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
