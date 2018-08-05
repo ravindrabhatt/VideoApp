837dbd7042d46fa1cd5e6839d571a6b57dd6d692
Ravindra Bhatt <ravindra.bhatt29@gmail.com>
2018-08-05 11:41:13 +0530

    Add styling to search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,9 +9,11 @@ class SearchBar extends Component {
 
 	render() {
 	  return (
-	  	<input 
-	  	  value={this.state.term}   
-	  	  onChange= {event => this.setState({ term: event.target.value })} />
+	  	<div className="search-bar">
+	  	  <input 
+	  	    value={this.state.term}   
+	  	    onChange= {event => this.setState({ term: event.target.value })} />
+	  	</div>
 	  );
   }
 };
